e4cc315ac5e7b12dc06cbad54f2640659c173af4
Yifei Yin <yifeiyin@foxmail.com>
2018-01-01 21:25:23 -0800

    It works!!!

---
+++ main.cpp
@@ -0,0 +1,344 @@
+#include <iostream>
+#include <vector>
+#include <string>
+#include <ctime>
+
+using std::cout;
+using std::endl;
+using std::string;
+using std::vector;
+
+const int disk_num = 6;
+const int peg_num = 3;
+
+class Movement
+{
+public:
+    int from;
+    int to;
+
+    Movement()
+    {
+        from = to = 0;
+    }
+
+    Movement(int f, int t)
+    {
+        from = f; to = t;
+    }
+
+    bool operator==(Movement & m)
+    {
+        return (from == m.from) && (to == m.to);
+    }
+
+    string GetString()
+    {
+        return std::to_string(from) + " -> " + std::to_string(to);
+    }
+};
+
+class Record
+{
+public:
+    vector<Movement> movements;
+    bool solved;
+
+    Record()
+    {
+
+    }
+
+    Record(vector<Movement> m, bool s)
+    {
+        movements = m; solved = s;
+    }
+
+    Record(bool s)
+    {
+        solved = s;
+    }
+};
+
+class Peg
+{
+public:
+    int stack[disk_num];
+
+    Peg()
+    {
+        for (int i = 0; i < disk_num; i++)
+            stack[i] = 0;
+    }
+
+    bool IsEmpty()
+    {
+        for (int i = 0; i < disk_num; i++)
+            if (stack[i] != 0)
+                return false;
+        return true;
+    }
+
+    int GetTop()
+    {
+        for (int i = disk_num - 1; i >= 0; i--)
+            if (stack[i] != 0)
+            {
+                return stack[i];
+            }
+        return 0;
+    }
+
+    int Pop()
+    {
+        for (int i = disk_num - 1; i >= 0; i--)
+            if (stack[i] != 0)
+            {
+                int tmp = stack[i];
+                stack[i] = 0;
+                return tmp;
+            }
+        return 0;
+    }
+
+    bool Push(int disk)
+    {
+        for (int i = 0; i < disk_num; i++)
+        {
+            if (stack[i] == 0)
+            {
+                bool tmp = i != 0 && stack[i-1] > disk;
+                bool tmp2 = i == 0;
+                if (tmp || tmp2)
+                {
+                    stack[i] = disk;
+                    return true;
+                }
+                else
+                {
+                    return false;
+                }
+            }
+        }
+        return false;
+    }
+
+    Peg (Peg & op)
+    {
+        for (int i = 0; i < disk_num; i++)
+            stack[i] = op.stack[i];
+    }
+
+    Peg & operator=(Peg & op)
+    {
+        if (*this == op)
+            return *this;
+
+        for (int i = 0; i < disk_num; i++)
+            this->stack[i] = op.stack[i];
+        return *this;
+    }
+
+    bool operator==(Peg & op)
+    {
+        for (int i = 0; i < disk_num; i++)
+            if (stack[i] != op.stack[i])
+                return false;
+        return true;
+    }
+
+    friend bool Move(Peg & from, Peg & to)
+    {
+        if (from.IsEmpty())
+            return false;
+
+        int disk = from.Pop();
+        if (to.Push(disk))
+            return true;
+        else
+            from.Push(disk);
+
+        return false;
+    }
+
+    bool MoveTo(Peg & to)
+    {
+        return Move(*this, to);
+    }
+};
+
+class Pegs
+{
+public:
+    Peg pegs[peg_num];
+
+    Pegs()
+    {
+        pegs[0].Push(5);
+        pegs[0].Push(4);
+        pegs[0].Push(3);
+        pegs[0].Push(2);
+        pegs[0].Push(1);
+    }
+
+    void Print()
+    {
+        for (int i = disk_num - 1; i >= 0 ; i--)
+        {
+            for (int j = 0; j < peg_num; j++)
+            {
+                cout << " |";
+                if  (pegs[j].stack[i] == 0) cout << "   ";
+                else                        cout << "-" << pegs[j].stack[i] << "-";
+                cout << "|   ";
+            }
+            cout << endl;
+        }
+    }
+
+    bool Move(int from, int to)
+    {
+//        this->Print(); //DEBUG
+//        cout << from << " " << to << endl; //DEBUG
+//        bool tmp = pegs[from].MoveTo(pegs[to]); //DEBUG
+//        this->Print(); //DEBUG
+//        cout << endl; //DEBUG
+//        return tmp; //DEBUG
+        return pegs[from].MoveTo(pegs[to]);
+    }
+
+    Pegs(Pegs & op)
+    {
+        for (int i = 0; i < peg_num; i++)
+            pegs[i] = op.pegs[i];
+    }
+
+    Pegs & operator=(Pegs & op)
+    {
+        if (&op == this)
+            return *this;
+
+        for (int i = 0; i < peg_num; i++)
+            this->pegs[i] = op.pegs[i];
+        return *this;
+    }
+
+    bool IsWhatWeWant()
+    {
+        Peg empty, full;
+        full.Push(5);
+        full.Push(4);
+        full.Push(3);
+        full.Push(2);
+        full.Push(1);
+
+        if (pegs[0] == empty && (/*pegs[1] == full || */pegs[2] == full))
+            return true;
+        return false;
+    }
+
+
+
+    Record Solve(vector<Movement> movements_so_far, Pegs pegs_so_far, int search_limit, unsigned long initial_time, unsigned long time_limit)
+    {
+        if (pegs_so_far.IsWhatWeWant())
+            return Record(movements_so_far, true);
+
+        if ((int)movements_so_far.size() >= search_limit)
+        {
+            cout << "Search limits exceeded: "; //DEBUG
+            for (unsigned long i = 0; i < movements_so_far.size(); i++) //DEBUG
+                cout << movements_so_far[i].GetString() << "  "; //DEBUG
+            cout << endl;
+            this->Print(); //DEBUG
+            cout << endl; //DEBUG
+            return Record(movements_so_far, false);
+        }
+
+        if (clock() - initial_time > time_limit)
+        {
+            cout << "Time limits exceeded\n"; // DEBUG
+            return Record(movements_so_far, false);
+        }
+
+        for (int from = 0; from < peg_num; from++)
+        {
+            for (int to = 0; to < peg_num; to++)
+            {
+                if (from == to)
+                    continue;
+                if (!movements_so_far.empty())
+                {
+//                    if (movements_so_far.back().from == to &&
+//                            movements_so_far.back().to == from)
+//                        continue;
+
+                    if (movements_so_far.back().to == from)
+                        continue;
+
+                    if (movements_so_far.size() >= 2)
+                        if (movements_so_far[movements_so_far.size()-2].from == to &&
+                                movements_so_far[movements_so_far.size()-2].to == from)
+                            continue;
+
+                }
+
+                Pegs p = pegs_so_far;
+
+                if (p.Move(from, to))
+                {
+                    system("clear");
+                    cout << "A valid move has been found: " << from << " -> " << to << endl; //DEBUG
+                    cout << "Previous moves: ";// DEBUG
+                    for (unsigned long debug = 0; debug < movements_so_far.size(); debug++)// DEBUG
+                        cout << movements_so_far[debug].GetString() << " ";// DEBUG
+                    cout << "Current status:" << endl;// DEBUG
+                    p.Print();// DEBUG
+                    cout << endl; // DEBUG
+                    vector<Movement> m = movements_so_far;
+                    m.push_back(Movement(from, to));
+
+                    Record r;
+                    r = Solve(m, p, search_limit, initial_time, time_limit);
+
+                    if (r.solved == true)
+                        return r;
+                }
+            }
+        }
+        return Record (false);
+    }
+};
+
+int main()
+{
+    Pegs p;
+    p.Print();
+
+//    Pegs b;
+//    b.Print();
+
+//    cout << b.Move(0,2);
+
+//    cout << endl;
+//    b.Print();
+
+//    p = b;
+//    cout << endl;
+//    p.Print();
+
+    Pegs a;
+    Record main_r = p.Solve({}, p, 40, clock(), 30000);
+
+    if (main_r.solved)
+            cout << "Solution found:\n";
+    else
+            cout << "Failed to find a solution\n";
+
+    for (unsigned long i = 0; i < main_r.movements.size(); i++)
+        cout << main_r.movements[i].GetString() << " ";
+
+    cout << endl;
+
+    return 0;
+}
--- main2.cpp
+++ main2.cpp
@@ -1,208 +1,202 @@
-#include <iostream>
-#include <vector>
-using std::cout;
-using std::endl;
-using std::vector;
+//#include <iostream>
+//#include <vector>
+//using std::cout;
+//using std::endl;
+//using std::vector;
 
-const int disk_num = 5;
-const int peg_num = 3;
+//const int disk_num = 5;
+//const int peg_num = 3;
 
-class struct_move
-{
-public:
-    int from = 0;
-    int to = 0;
+//class struct_move
+//{
+//public:
+//    int from = 0;
+//    int to = 0;
 
-    bool operator==(struct_move & s)
-    {
-        return ((from == s.from) && (to == s.to));
-    }
-};
+//    bool operator==(struct_move & s)
+//    {
+//        return ((from == s.from) && (to == s.to));
+//    }
+//};
 
-class Peg
-{
-public:
-    int stack [disk_num];
+//class Peg
+//{
+//public:
+//    int stack [disk_num];
 
-    Peg()
-    {
-        for (int i = 0; i < disk_num; i++)
-            stack[i] = 0;
-    }
+//    Peg()
+//    {
+//        for (int i = 0; i < disk_num; i++)
+//            stack[i] = 0;
+//    }
 
-    bool isEmpty()
-    {
-        for (int i = 0; i < disk_num; i++)
-            if (stack[i] != 0)
-                return false;
-        return true;
-    }
+//    bool isEmpty()
+//    {
+//        for (int i = 0; i < disk_num; i++)
+//            if (stack[i] != 0)
+//                return false;
+//        return true;
+//    }
 
-    int pop()
-    {
-        for (int i = disk_num - 1; i >= 0; i--)
-            if (stack[i] != 0)
-            {
-                int tmp = stack[i];
-                stack[i] = 0;
-                return tmp;
-            }
-        return 0;
-    }
+//    int pop()
+//    {
+//        for (int i = disk_num - 1; i >= 0; i--)
+//            if (stack[i] != 0)
+//            {
+//                int tmp = stack[i];
+//                stack[i] = 0;
+//                return tmp;
+//            }
+//        return 0;
+//    }
 
-    bool push(int disk)
-    {
-        for (int i = 0; i < disk_num; i++)
-        {
-            if (stack[i] == 0)
-            {
-                bool tmp = stack[i] != 0 && stack[i-1] < disk;
-                bool tmp2 = stack[i] == 0;
-                if (tmp || tmp2)
-                {
-                    stack[i] = disk;
-                    return true;
-                }
-                else
-                {
-                    return false;
-                }
-            }
-        }
-        return false;
-    }
+//    bool push(int disk)
+//    {
+//        for (int i = 0; i < disk_num; i++)
+//        {
+//            if (stack[i] == 0)
+//            {
+//                bool tmp = stack[i] != 0 && stack[i-1] < disk;
+//                bool tmp2 = stack[i] == 0;
+//                if (tmp || tmp2)
+//                {
+//                    stack[i] = disk;
+//                    return true;
+//                }
+//                else
+//                {
+//                    return false;
+//                }
+//            }
+//        }
+//        return false;
+//    }
 
-    Peg & operator=(Peg & op)
-    {
-        Peg * out = new Peg;
-        for (int i = 0; i < disk_num; i++)
-            op.stack[i] = out->stack[i];
-        return *out;
-    }
+//    Peg & operator=(Peg & op)
+//    {
+//        Peg * out = new Peg;
+//        for (int i = 0; i < disk_num; i++)
+//            op.stack[i] = out->stack[i];
+//        return *out;
+//    }
 
-    friend bool Move(Peg & from, Peg & to)
-    {
-        if (from.isEmpty())
-            return false;
+//    friend bool Move(Peg & from, Peg & to)
+//    {
+//        if (from.isEmpty())
+//            return false;
 
-        int disk = from.pop();
-        if (to.push(disk))
-            return true;
-        else
-            from.push(disk);
+//        int disk = from.pop();
+//        if (to.push(disk))
+//            return true;
+//        else
+//            from.push(disk);
 
-        return true;
-    }
-};
+//        return true;
+//    }
+//};
 
-void Print(Peg * p, int peg_num, vector<struct_move> moves = * new vector<struct_move>)
-{
-    if (!moves.empty())
-    {
-        for (int i = 0; i < (int)moves.size(); i++)
-        {
-            if (!Move(p[moves[i].from], p[moves[i].to]))
-            {
-                    cout << "error\n";
-            }
-        }
-    }
-    for (int i = disk_num - 1; i >= 0 ; i--)
-    {
-        for (int j = 0; j < peg_num; j++)
-        {
-            cout << " |";
-            if  (p[j].stack[i] == 0) cout << "   ";
-            else                     cout << "-" << p[j].stack[i] << "-";
-            cout << "|   ";
-        }
-        cout << endl;
-    }
-}
+//void Print(Peg * p, int peg_num, vector<struct_move> moves = {})
+//{
+//    if (!moves.empty())
+//    {
+//        for (int i = 0; i < (int)moves.size(); i++)
+//        {
+//            if (!Move(p[moves[i].from], p[moves[i].to]))
+//            {
+//                    cout << "error\n";
+//            }
+//        }
+//    }
+//    for (int i = disk_num - 1; i >= 0 ; i--)
+//    {
+//        for (int j = 0; j < peg_num; j++)
+//        {
+//            cout << " |";
+//            if  (p[j].stack[i] == 0) cout << "   ";
+//            else                     cout << "-" << p[j].stack[i] << "-";
+//            cout << "|   ";
+//        }
+//        cout << endl;
+//    }
+//}
 
-bool Solve_outside(Peg * pegs, int peg_num);
-vector<struct_move> Solve(vector<struct_move> moves_so_far, Peg* pegs, int peg_num, int moves_limit, long initial_time);
+//bool Solve_outside(Peg * pegs, int peg_num);
+//vector<struct_move> Solve(vector<struct_move> moves_so_far, Peg* pegs, int peg_num, int moves_limit, long initial_time);
 
-int main()
-{
-    Peg pegs [3];
-    pegs[0].push(5);
-    pegs[0].push(4);
-    pegs[0].push(3);
-    pegs[0].push(2);
-    pegs[0].push(1);
+//int main2()
+//{
+//    Peg pegs [3];
+//    pegs[0].push(5);
+//    pegs[0].push(4);
+//    pegs[0].push(3);
+//    pegs[0].push(2);
+//    pegs[0].push(1);
 
-    Print(pegs, 3);
+//    Print(pegs, 3);
 
-    vector<struct_move> m = Solve({}, pegs, 3, 100, 0);
+//    vector<struct_move> m = Solve({}, pegs, 3, 100, 0);
 
-    for (int i = 0; i < m.size(); i++)
-        cout << i << " :  " << m[i].from << " -> " << m[i].to << endl;
+//    for (int i = 0; i < m.size(); i++)
+//        cout << i << " :  " << m[i].from << " -> " << m[i].to << endl;
 
-    return 0;
-}
+//    return 0;
+//}
 
-bool Solve_outside(Peg * pegs, int peg_num)
-{
-//    vector<struct_move> empty;
-//    vector<struct_move> solution = Solve(empty, pegs, peg_num, 100, (long)ctime);
-    return true;
-}
+//bool Solve_outside(Peg * pegs, int peg_num)
+//{
+////    vector<struct_move> empty;
+////    vector<struct_move> solution = Solve(empty, pegs, peg_num, 100, (long)ctime);
+//    return true;
+//}
 
-vector<struct_move> Solve(vector<struct_move> moves_so_far, Peg* pegs, int peg_num, int moves_limit, long initial_time)
-{
-    for (int i_from = 0; i_from < peg_num; i_from++)
-        for (int i_to = 0; i_to < peg_num; i_to++)
-        {
-            if (i_from == i_to)
-                continue;
-            struct_move tmp;
-            tmp.to = i_from;
-            tmp.from = i_to;
-            if (moves_so_far.back() == tmp)
-                continue;
+//vector<struct_move> Solve(vector<struct_move> moves_so_far, Peg* pegs, int peg_num, int moves_limit, long initial_time)
+//{
+//    for (int i_from = 0; i_from < peg_num; i_from++)
+//        for (int i_to = 0; i_to < peg_num; i_to++)
+//        {
+//            if (i_from == i_to)
+//                continue;
+//            struct_move tmp;
+//            tmp.to = i_from;
+//            tmp.from = i_to;
+//            if (moves_so_far.back() == tmp)
+//                continue;
 
-            if ((int)moves_so_far.size() >= moves_limit)
-            {
-                cout << "Moves exceeds limits.\n";
-            }
+//            if ((int)moves_so_far.size() >= moves_limit)
+//            {
+//                cout << "Moves exceeds limits.\n";
+//            }
 
-            if (pegs[0].isEmpty())
-                return moves_so_far;
+//            if (pegs[0].isEmpty())
+//                return moves_so_far;
 
-            Peg * pegs2 = new Peg [peg_num];
-            for (int i = 0; i < peg_num; i++)
-                pegs2[i] = pegs[i];
+//            Peg * pegs2 = new Peg [peg_num];
+//            for (int i = 0; i < peg_num; i++)
+//                pegs2[i] = pegs[i];
 
-            if (Move(pegs[i_from], pegs[i_to]))
-            {
-                struct_move tmp;
-                tmp.from = i_from;
-                tmp.to = i_to;
+//            if (Move(pegs[i_from], pegs[i_to]))
+//            {
+//                struct_move tmp;
+//                tmp.from = i_from;
+//                tmp.to = i_to;
 
-                vector<struct_move> moves_so_far2;
-                moves_so_far2 = moves_so_far;
-                moves_so_far2.push_back(tmp);
+//                vector<struct_move> moves_so_far2;
+//                moves_so_far2 = moves_so_far;
+//                moves_so_far2.push_back(tmp);
 
-                Solve(moves_so_far2, pegs2, peg_num, moves_limit, initial_time);
-            }
-        }
-    return {};
-}
+//                Solve(moves_so_far2, pegs2, peg_num, moves_limit, initial_time);
+//            }
+//        }
+//    return {};
+//}
 
-class Pegs
-{
-public:
-    Peg pegs[peg_num];
+//class Pegs
+//{
+//public:
+//    Peg pegs[peg_num];
 
-    bool Move(int from, int to)
-    {
-        if (from < 0 || from >= peg_num || to < 0 || to >= peg_num)
-        {
-            cout << "error 202\n";
-            return false;
-        }
+////    Print()
+////    {
 
-        return Move(pegs[from], pegs[to]);
-    }
-};
+////    }
+//};
